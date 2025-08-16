749581f775ffc5949f295aac26050b2eed1bf062
GUNW-O-O <rhrjsdn12@naver.com>
2025-08-17 00:27:55 +0900

    NestJS

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('quiz')
-export class QuizController {}
+export class QuizController {
+  @Get()
+  getQuizzes(): String[] {
+    return ['TypeScript', 'JavaScript', 'React', 'NestJS'];
+  }
+}
