39a95d7a3bce88875b6c83a3e48cb5b4514b5ddd
GUNW-O-O <rhrjsdn12@naver.com>
2025-09-24 16:46:10 +0900

    aws

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; // ValidationPipe 임포트
-import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe()); // 전역 파이프 추가
-  // 동적 임포트를 사용하여 cookieParser 미들웨어를 적용합니다.
-  app.use(cookieParser.default());
-  // CORS 설정
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://lexihub.s3-website.ap-northeast-2.amazonaws.com'
+    ],
     credentials: true,
   });
-  
-  await app.listen(process.env.PORT ?? 3000);
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
